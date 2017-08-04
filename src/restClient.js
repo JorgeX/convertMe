8dad38bc7ef91f6c33bcc290ca0d41c3f3f74592
Jörge Ojanen <jorge@Jorges-MacBook-Pro.local>
2017-08-04 11:30:26 +0300

    added restclient

---
+++ src/restClient.js
@@ -0,0 +1,10 @@
+/**
+ * Obviously, for the rest calls
+ * get some apis here:
+ * ** Currency Courses **
+ *   http://api.fixer.io/latest
+ * 
+ * 
+ */
+
+ let currencyUrl = 'http://api.fixer.io/latest';
