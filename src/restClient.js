b628cf2d9fca00f22781c7216da082bc09d0ab4a
Jörge Ojanen <jorge.mhnet@gmail.com>
2017-08-04 13:51:55 +0300

    get methods to restclient

--- src/restClient.js
+++ src/restClient.js
@@ -4,3 +4,29 @@
 
  const currencyUrl = 'http://api.fixer.io/latest';
  const taxRatesUrl = 'https://developer.avalara.com/api-reference/avatax/rest/v1/';
+
+ export default class RestClient{
+
+    static getCurrencyRates(success, failure, currencies){
+        //TODO
+    }
+
+    static getSalesTaxPercentage(success, failure, state){
+        //TODO
+    }
+ }
+
+ export function ajaxGet(url, data, successcb, failurecb){
+     //cache?true
+     return $.ajax({
+        type:'GET',
+        url: url,
+        data: data,
+        success: function(response){
+            successcb(response);
+        },
+        failure: function(err){
+            failurecb(err);
+        }
+     });
+ }
