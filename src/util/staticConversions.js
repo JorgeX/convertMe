f56126e2b31ec215eb572ca1b7cf4fb21518c4ff
Jörge Ojanen <jorge.mhnet@gmail.com>
2017-08-06 22:56:09 +0300

    todo conversion units

--- src/util/staticConversions.js
+++ src/util/staticConversions.js
@@ -7,3 +7,11 @@ const POUNDLBS_TO_KG = 0.45;
 /** liquid volume */
 const GALLON_TO_LITRE = 3.79;
 const LITRE_TO_GALLON = 0.26;
+
+/** distances */
+const INCH_TO_CM = 0;
+const CM_TO_INCH = 0;
+const MILE_TO_KM=0;
+const KM_TO_MILE=0;
+const FOOT_TO_CM=0;
+const CM_TO_FOOT=0;
